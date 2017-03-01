f1096393221b08fdb1a3e325e6458c7b7c3d8e63
Kok Yong <KokYong@Koks-MacBook-Pro.local>
2017-03-01 13:06:18 +0800

    file added

---
+++ medR/PatientProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PatientProfileViewController.swift
+//  medR
+//
+//  Created by Kok Yong on 01/03/2017.
+//  Copyright © 2017 Kok Yong. All rights reserved.
+//
+
+import UIKit
+
+class PatientProfileViewController: UIViewController {
+
+    
+    //viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}
