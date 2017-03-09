af271589e980d386145d78565ba7053515d2552e
george chin fu hou <georgechinfuhou@georges-MacBook-Air.local>
2017-03-09 18:33:14 +0800

    google and facebook login

--- medR/AppDelegate.swift
+++ medR/AppDelegate.swift
@@ -9,9 +9,11 @@
 import UIKit
 import CoreData
 import Firebase
+import FBSDKCoreKit
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     var window: UIWindow?
     
@@ -21,10 +23,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FIRApp.configure()
         
         // displayKYController()
-        displayRuiController()
-        //displayLogin()
+        //displayRuiController()
+        displayLogin()
+        
+        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         
         return true
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let err = error {
+            print("Failed to log into Google", err)
+        return
+        }
+            print("Successfully logged into Google", user)
+        
+        guard let idToken = user.authentication.idToken else { return }
+        guard let accessToken = user.authentication.accessToken else { return }
+        let credentials = FIRGoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        FIRAuth.auth()?.signIn(with: credentials, completion: {(user, error) in
+            if let err = error {
+                print("Failed to create Firebase User with Google account: ", err)
+                return
+            }
+            guard let uid = user?.uid else {return}
+            print("Successfully logged into Firebase with Google", uid)
+    })
+    
+}
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+    
+        GIDSignIn.sharedInstance().handle(url,
+                                          sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!,
+                                          annotation: options[UIApplicationOpenURLOptionsKey.annotation] )
+        
+        return handled
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
--- medR/LoginViewController.swift
+++ medR/LoginViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 import FirebaseAuth
 import FirebaseDatabase
 import FBSDKLoginKit
+import Firebase
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDelegate  {
     
     
-    
+   
     @IBOutlet weak var userEmail: UITextField!
     
     @IBOutlet weak var userPassword: UITextField!
@@ -32,10 +35,14 @@ class LoginViewController: UIViewController {
     
         navigationController?.pushViewController(controller, animated: true)
     }
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupFacebookButton()
+        setupGoogleButton()
+        
         
         self.navigationItem.setHidesBackButton(true, animated:true);
 
@@ -53,10 +60,77 @@ class LoginViewController: UIViewController {
         userPassword.layer.borderWidth = 2
         userPassword.layer.cornerRadius = 8
     
-       
+    
         
         
+    
+    }
+    
+    fileprivate func setupFacebookButton() {
+        //add facebook sign in button
+        let loginButton = FBSDKLoginButton()
+        view.addSubview(loginButton)
+        // frame are obselete
+        loginButton.frame = CGRect(x: 16, y: 430, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
+        
+        loginButton.readPermissions = ["email", "public_profile"]
+        
+
+    }
+    
+    fileprivate func setupGoogleButton() {
+        //add google sign in button
+        let googleButton = GIDSignInButton()
+        googleButton.frame = CGRect(x: 16, y: 520, width: view.frame.width - 32, height: 50)
+        view.addSubview(googleButton)
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+        
+    }
+    
+        
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+            
         }
+        showEmailAddress()
+    }
+    
+    func showEmailAddress() {
+        
+        let accessToken = FBSDKAccessToken.current()
+        guard let  accessTokenString = accessToken?.tokenString else {return}
+        
+        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            if error != nil {
+                print ("Something went wrong with our FB user: ", error ?? "")
+                return
+            }
+            print ("Successfully logged in with our user", user ?? "")
+            
+            })
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start {
+        (connection, result, err) in
+        
+            if err != nil {
+                print("Failed to start graph request", err)
+                return
+            }
+            
+            print(result ?? "")
+    }
+    }
+
     // Login Function
     func login () {
         FIRAuth.auth()?.signIn(withEmail: userEmail.text!, password: userPassword.text!, completion: {(user,error) in
@@ -83,9 +157,7 @@ class LoginViewController: UIViewController {
     }
 
     
-    }
-
-
+}
 
 
 
