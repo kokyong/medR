65cb6af62177d77879f82bd20999513768ceea96
Rui Ong <rui.ong91@gmail.com>
2017-03-14 15:55:34 +0800

    solve podfile issue and add gitignore

--- Pods/FBSDKCoreKit/FBSDKCoreKit/FBSDKCoreKit/FBSDKCoreKit.h
+++ Pods/FBSDKCoreKit/FBSDKCoreKit/FBSDKCoreKit/FBSDKCoreKit.h
@@ -44,9 +44,5 @@
 #import <FBSDKCoreKit/FBSDKDeviceViewControllerBase.h>
 #endif
 
-<<<<<<< HEAD
 #define FBSDK_VERSION_STRING @"4.20.1"
-=======
-#define FBSDK_VERSION_STRING @"4.20.0"
->>>>>>> 8a4b5126cae96291985236d9f4763f37c99dea3c
 #define FBSDK_TARGET_PLATFORM_VERSION @"v2.8"
